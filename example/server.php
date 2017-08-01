a9dd5efde52632b34f0f7b3e689bd9bba79dded1
three <three.benji@gmail.com>
2017-08-01 16:14:55 +0800

    fix

--- example/server.php
+++ example/server.php
@@ -2,8 +2,6 @@
 
 use ThreeFrame\Config;
 use ThreeFrame\Http;
-use ThreeFrame\ORM;
-
 require_once __DIR__."/vendor/autoload.php";
 require __DIR__."/app/routes.php";
 Config::Init(__DIR__."/config.ini");
